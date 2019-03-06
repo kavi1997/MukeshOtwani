b464fb35e9d0c040e9a3b905b11becd28cf3a397
Gauranga <Gauranga@DESKTOP-5GU4J3U.Home>
2019-03-06 18:35:24 +0000

    Otwani framwork commit

---
+++ MukeshOtwaniAutomationFramwork/src/test/java/stepDefiniton/SmokeTest.java
@@ -0,0 +1,43 @@
+package stepDefiniton;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SmokeTest {
+	
+	WebDriver driver;
+	
+	@Given("Open chrome and start application")
+	public void open_firefox_and_start_application() {
+		//System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self'; style-src 'self' 'unsafe-inline'; font-src *");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Gauranga\\Documents\\Praveen\\MukeshOtwaniAutomationFramwork\\drivers\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.facebook.com");
+}
+	    
+	@When("I enter valid {string} and valid {string}")
+	public void i_enter_valid_and_valid(String uname, String upass) {
+		driver.findElement(By.id("email")).sendKeys(uname);
+		driver.findElement(By.id("pass")).sendKeys(upass);   
+	}
+
+	@Then("user should be able to login successfully")
+	public void user_should_be_able_to_login_successfully() {
+		//driver.findElement(By.xpath("//input[@type='submit'][@id='u_0_3']")).click();
+	    
+	}
+	
+	@Then("close the browser")
+	public void close_the_browser() {
+	driver.close();
+	}
+
+
+}
